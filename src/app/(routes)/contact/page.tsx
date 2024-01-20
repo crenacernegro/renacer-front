6c3525de3f8652a26be47fe91095ea6a42c05e9e
crenacernegro <crenacernegro@gmail.com>
2024-01-19 22:37:51 -0500

    added donate button

--- src/app/(routes)/contact/page.tsx
+++ src/app/(routes)/contact/page.tsx
@@ -3,16 +3,17 @@ import { FaPhoneVolume } from "react-icons/fa6";
 import { FaMailBulk } from "react-icons/fa";
 import { FaClock } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
-import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
+import { CiHeart } from "react-icons/ci";
 
 const ContactPage = () => {
   return (
     <>
-      <div className="mt-24 mb-4 grid grid-cols-1 gap-x-20 w-2/3 m-auto p-4 md:grid md:grid-cols-3 md:gap-20">
+      <div className="mt-24 mb-4 grid grid-cols-1 w-3/4  m-auto p-4 md:grid md:grid-cols-3 md:gap-20">
         <div className="mb-8">
           <p className="text-4xl font-bold text-lime-950 mb-4">CONTACTO:</p>
           <br />
-          <div className="grid grid-cols-2 gap-8 mt-8">
+          <div className="grid grid-cols-2 grid-rows-2 gap-8 mt-8 gap-x-0">
             <FaPhoneVolume
               className="text-4xl text-lime-950"
               style={{ marginTop: "1rem" }}
@@ -38,11 +39,16 @@ const ContactPage = () => {
             </div>
           </div>
         </div>
-        <br />
+
         <div className="mt-4">
           <ContactForm />
         </div>
-        <div>3</div>
+        <div>
+          <Button className="mt-8 text-xl ml-8">
+            <p className="px-2">¡Dona ahora!</p>
+            <CiHeart className="text-2xl" />
+          </Button>
+        </div>
       </div>
     </>
   );
