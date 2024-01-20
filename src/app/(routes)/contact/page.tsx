cdeeaa0b1347bde72474d07d0c59447036de7cb6
crenacernegro <crenacernegro@gmail.com>
2024-01-19 22:46:53 -0500

    finished contact page

--- src/app/(routes)/contact/page.tsx
+++ src/app/(routes)/contact/page.tsx
@@ -9,31 +9,31 @@ import { CiHeart } from "react-icons/ci";
 const ContactPage = () => {
   return (
     <>
-      <div className="mt-24 mb-4 grid grid-cols-1 w-3/4  m-auto p-4 md:grid md:grid-cols-3 md:gap-20">
+      <div className="mt-24 mb-4 grid grid-cols-1 w-4/5  m-auto p-4 md:grid md:grid-cols-3 md:gap-20">
         <div className="mb-8">
           <p className="text-4xl font-bold text-lime-950 mb-4">CONTACTO:</p>
           <br />
-          <div className="grid grid-cols-2 grid-rows-2 gap-8 mt-8 gap-x-0">
+          <div className="grid grid-cols-2 gap-4 mt-8 gap-x-0">
             <FaPhoneVolume
-              className="text-4xl text-lime-950"
+              className="text-4xl text-lime-950 ml-4"
               style={{ marginTop: "1rem" }}
             />
-            <div className="">3167864270</div>
+            <div className="mt-4">3167864270</div>
             <FaMailBulk
-              className="text-4xl text-lime-950"
+              className="text-4xl text-lime-950 ml-4"
               style={{ marginTop: "1rem" }}
             />
-            <div>crenacernegro@gmail.com</div>
+            <div className="mt-4">crenacernegro@gmail.com</div>
             <FaClock
-              className="text-4xl text-lime-950"
+              className="text-4xl text-lime-950 ml-4"
               style={{ marginTop: "1rem" }}
             />
-            <div>Lunes a Viernes entre 8:00 am y 5:30 pm</div>
+            <div className="mt-4">Lunes a Viernes entre 8:00 am y 5:30 pm</div>
             <FaLocationDot
-              className="text-4xl text-lime-950"
+              className="text-4xl text-lime-950 ml-4"
               style={{ marginTop: "1rem" }}
             />
-            <div>
+            <div className="mt-4">
               <p>Barrio San Jose</p>
               <p>Timbiquí-Cauca</p>
             </div>
@@ -46,7 +46,7 @@ const ContactPage = () => {
         <div>
           <Button className="mt-8 text-xl ml-8">
             <p className="px-2">¡Dona ahora!</p>
-            <CiHeart className="text-2xl" />
+            <CiHeart className="text-2xl " />
           </Button>
         </div>
       </div>
