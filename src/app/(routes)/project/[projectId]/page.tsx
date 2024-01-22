507ea0ceb2865686ab89a4fbeefc5f487ab7e8d5
crenacernegro <crenacernegro@gmail.com>
2024-01-22 13:17:38 -0500

    finished project page

--- src/app/(routes)/project/[projectId]/page.tsx
+++ src/app/(routes)/project/[projectId]/page.tsx
@@ -19,8 +19,8 @@ const ProjectPage: React.FC<ProjectPageProps> = async ({ params }) => {
   return (
     <div className="bg-white">
       <Container>
-        <div className="p-0 mb-60">
-          <div className="w-full flex">
+        <div className="p-0 mb-80">
+          <div className="w-full flex mt-20">
             <Image
               src={project.images[0].url}
               alt={project.name}
@@ -30,15 +30,15 @@ const ProjectPage: React.FC<ProjectPageProps> = async ({ params }) => {
               objectFit="cover"
             />
           </div>
-          <div className="absolute h-80 w-2/3 ml-32 bg-lime-100 -mt-24">
-            <div className="ml-24 text-lime-950 text-4xl font-bold mt-8">
-              {project.name}
+          <div className="absolute h-80 w-3/4 ml-12 bg-lime-100 -mt-16 rounded-xl md:-mt-48">
+            <div className="ml-4 text-lime-950 text-md font-bold p-4 text-center md:text-lg">
+              <p className="md:text-2xl">{project.name}</p>
             </div>
-            <div className="absolute ml-24 mt-24">{project.description}</div>
-            <div className="absolute ml-24 mt-36 font-semibold">
+            <div className="ml-4 text-sm p-4">{project.description}</div>
+            <div className="ml-8 font-medium text-sm p-0">
               Municipios: {project.location}
             </div>
-            <div className="absolute ml-24 mt-44 font-semibold">
+            <div className="font-medium ml-8 text-sm p-0">
               Cantidad de participantes: {project.numberOfParticipants}
             </div>
           </div>
