758fa3f67c1f9e9da5ba85c40b9ecc1e16bf73b2
crenacernegro <crenacernegro@gmail.com>
2024-02-01 15:30:14 -0500

    changed project page

--- src/app/(routes)/project/[projectId]/page.tsx
+++ src/app/(routes)/project/[projectId]/page.tsx
@@ -30,15 +30,21 @@ const ProjectPage: React.FC<ProjectPageProps> = async ({ params }) => {
               objectFit="cover"
             />
           </div>
-          <div className="absolute h-80 w-3/4 ml-12 bg-lime-100 -mt-16 rounded-xl md:-mt-48">
+          <div className="absolute h-80 w-3/4 ml-12 bg-lime-100 -mt-16 rounded-xl md:-mt-48 md:w-1/3 md:mx-80">
             <div className="ml-4 text-lime-950 text-md font-bold p-4 text-center md:text-lg">
               <p className="md:text-2xl">{project.name}</p>
             </div>
-            <div className="ml-4 text-sm p-4">{project.description}</div>
-            <div className="ml-8 font-medium text-sm p-0">
+            <div className="text-sm p-2 text-justify md:mx-8">
+              Fundamentar las competencias de los y las jóvenes de los Distritos
+              de Cali y Buenaventura en la gestión de lo público y de
+              iniciativas que permitan construir de manera democrática
+              soluciones a corto, mediano y largo plazo en el marco del Pacto
+              Colombia con las Juventudes y el CONPES 4040 de 2021.{" "}
+            </div>
+            <div className="ml-2 font-medium text-sm p-0 md:mx-10 mt-4">
               Municipios: {project.location}
             </div>
-            <div className="font-medium ml-8 text-sm p-0">
+            <div className="font-medium ml-2 text-sm p-0 md:mx-10 mt-4">
               Cantidad de participantes: {project.numberOfParticipants}
             </div>
           </div>
